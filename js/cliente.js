ce05ef5eeb0eb109e6bf8d50945e7cb8325c5fd8
Teddy Paulino <teddypaulino@gmail.com>
2024-03-19 20:46:50 -0400

    Ingresando JavaScript al formulario cliente

---
+++ js/cliente.js
@@ -0,0 +1,31 @@
+(function() {
+   formularioCliente.addEventListener('submit', agregarCliente)
+    agregarCliente();
+
+   function agregarCliente(e) {
+    e.preventDefault();
+
+    const nombreInput = document.querySelector("#nombrecliente").value;
+    const apellidoInput = document.querySelector("#apellidoCliente").value;
+    const dirInpunt = document.querySelector('#direccionCliente').value;
+    const telefonoInput = document.querySelector('#telefonoCliente').value;
+    const tipoDocInput = document.querySelector('#tipoDocumentCliente').value;
+    const cedulaInput = document.querySelector('#cedulaCliente').value;
+    const emailInput = document.querySelector('#emailCliente').value;
+
+    if (nombreInput === '' || apellidoInput === '' || dirInpunt === '' || tipoDocInput === '' || cedulaInput === '') {
+        mostrarMensaje();
+    }else {
+
+    }   
+   }
+
+   function mostrarMensaje() {
+    const divMensaje = document.createElement('div');
+    divMensaje.classList.add('mensaje');
+    divMensaje.textContent = 'Los siguientes campos son obligatorios, Nombre, Apellido,Dirección, Tipo Documento, Cedula...'
+
+    formularioCliente.insertBefore(divMensaje, document.querySelector('form submit'));
+
+   }
+})();
